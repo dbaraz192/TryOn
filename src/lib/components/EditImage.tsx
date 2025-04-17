3868974ec931866bbf6be5596f4c42abe8cb106e
tetkob <Tetiana.Kobryn@mdlz.com>
2025-04-17 19:34:03 +0300

    refactor: Clean up EditImage component by removing unused imports and console logs

--- src/lib/components/EditImage.tsx
+++ src/lib/components/EditImage.tsx
@@ -6,8 +6,6 @@ import { useMutation } from "@tanstack/react-query";
 import { updateUserImage } from "../server/controllers/images";
 import { toast } from "sonner";
 import { Edit } from "lucide-react";
-// import { utapi } from "../server/uploadthing";
-import { getUploadThingFileKey } from "../utils/uploadthing";
 
 interface EditImageProps {
   type: 'frontUrl' | 'backUrl' | 'rightSideUrl' | 'leftSideUrl';
@@ -18,12 +16,7 @@ interface EditImageProps {
 export const EditImage = ({ type, imageUrl, label }: EditImageProps) => {
   const [image, setImage] = useState(imageUrl);
 
-  if (image) {
-    console.log("Image URL:", getUploadThingFileKey(image));
-  }
-
   const handleUploadComplete = async (res: Array<{ ufsUrl: string }>) => {
-    console.log("Upload complete:", res);
     if (res?.length > 0 && type) {
       const uploadedUrl = res[0].ufsUrl;
       setImage(uploadedUrl);
@@ -35,7 +28,6 @@ export const EditImage = ({ type, imageUrl, label }: EditImageProps) => {
       });
     }
   };
-
 
   const { mutate: submitImageUpdate } = useMutation({
     mutationFn: async ({
@@ -49,16 +41,6 @@ export const EditImage = ({ type, imageUrl, label }: EditImageProps) => {
       return await updateUserImage({ data });
     },
     onSuccess: async () => {
-      // const oldFileKey = imageUrl ? getUploadThingFileKey(imageUrl) : null;
-      // if (oldFileKey) {
-      //   try {
-      //     await utapi.deleteFiles(oldFileKey);
-      //     console.log("Old image deleted:", oldFileKey);
-      //   } catch (error) {
-      //     console.error("Failed to delete old image:", error);
-      //   }
-      // }
-  
       toast.success("Images uploaded successfully!");
     },
     onError: (error) =>
@@ -66,50 +48,50 @@ export const EditImage = ({ type, imageUrl, label }: EditImageProps) => {
   });
   
   return (
-    <Card className="flex items-center justify-center relative h-122 w-86">
+    <Card className="flex items-center justify-center relative max-h-122 max-w-86">
       {image && (
-        <>
-          <div className="relative h-full w-full">
-            <img
-              src={image}
-              alt={type}
-              className="mt-3 h-full w-full rounded-lg object-cover object-top"
-            />
-            <UploadButton<UploadRouter, "imageUploader">
-              endpoint="imageUploader"
-              onClientUploadComplete={(res) => handleUploadComplete(res)}
-              onUploadError={(error) => {
-                alert(`ERROR! ${error.message}`);
+        <div className="relative h-full w-full">
+          <img
+            src={image}
+            alt={type}
+            className="mt-3 h-full w-full rounded-lg object-cover object-top"
+          />
+          <UploadButton<UploadRouter, "imageUploader">
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => handleUploadComplete(res)}
+            onUploadError={(error) => {
+              toast.error(error.message);
+            }}
+            onUploadBegin={(name) => {
+              toast.info(`Uploading ${name}`);
+            }}
+            className="absolute top-5 flex flex-col items-end right-2 text-white"
+            content={{
+              button({ ready }) {
+                if (ready) return <Edit />;
+                return "Getting ready...";
+              },
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              allowedContent({ ready, isUploading }) {
+                // if (!ready) return "Checking what you allow";
+                // if (isUploading) return "Your photo is uploading";
+                return '';
+              },
+            }}
+            appearance={{
+              button: {
+                background: "black",
+                color: "white",
+                width: "40px"
+                
+              },
+              container: {
+                display: "flex",
+                background: "transparent",
               }}
-              onUploadBegin={(name) => {
-                console.log("Uploading: ", name);
-              }}
-              className="absolute top-5 flex flex-col items-end right-2 text-white"
-              content={{
-                button({ ready }) {
-                  if (ready) return <Edit />;
-                  return "Getting ready...";
-                },
-                allowedContent({ ready, isUploading }) {
-                  if (!ready) return "Checking what you allow";
-                  if (isUploading) return "Your photo is uploading";
-                  return '';
-                },
-              }}
-              appearance={{
-                button: {
-                  background: "black",
-                  color: "white",
-                  width: "40px"
-                },
-                container: {
-                  display: "flex",
-                  background: "transparent",
-                }}
-              }
-            />
-          </div>
-        </>
+            }
+          />
+        </div>
       )}
       <CardInnerTitle>
         {label.toUpperCase()}
